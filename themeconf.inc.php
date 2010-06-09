d21ab4e5b2a8d4c1b6541951831a0ff86d081097
plegall <plg@piwigo.org>
2010-06-09 23:30:03 +0000

    simple black, from Freddy Berriau, compatible with Piwigo 2.1
    git-svn-id: http://piwigo.org/svn/extensions/simple_themes/simple-black@6512 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Theme Name: Simple Black
+Version: 2.1a
+Description: Black (very dark gray) theme for Simple
+Theme URI: http://piwigo.org/ext/extension_view.php?eid=347
+Author: Fresdy
+Author URI: http://freddyberriau.com
+*/
+$themeconf = array(
+  'name'  => 'simple-black',
+  'parent' => 'simple',
+  );
+?>
